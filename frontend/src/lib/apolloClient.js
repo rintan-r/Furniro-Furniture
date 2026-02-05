e3a07ca6fb8c729092a2e98e2e7cb85e71b52ce5
rintan-r <dev.rintanrenatta@gmail.com>
2026-02-05 15:32:57 +0700

    update apolloclient

--- frontend/src/lib/apolloClient.js
+++ frontend/src/lib/apolloClient.js
@@ -1,8 +1,8 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
-    link: new HttpLink({
-        uri: 'http://localhost:4000',
-    }),
-    cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  }),
+  cache: new InMemoryCache(),
 });
