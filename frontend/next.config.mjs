08cdf495931dcd4fe77ebc5c389a5bbd06273c60
rintan-r <dev.rintanrenatta@gmail.com>
2026-02-05 10:25:11 +0700

    fix package

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,17 +1,24 @@
+import { fileURLToPath } from "url"
+import path from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-    images: {
+  turbopack: {
+    root: __dirname,
+  },
+  //reactCompiler: true,
+  images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**'
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
-    ]
+    ],
   },
-};
+}
 
-export default nextConfig;
-
+export default nextConfig
